40242d724cd577c7e9767998a7c48ddebd22f21c
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-01 22:49:36 +0300

    added tests for admin to get total number of men and total number of women

--- tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php
+++ tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php
@@ -30,4 +30,27 @@ class Counts_Controller_Tests extends TestCase
                    $response->assertOk();
             
                }
+
+        /** @test */
+        public function admin_can_get_total_number_of_men()
+        {
+    // clear && ./vendor/bin/phpunit tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php --filter admin_can_get_total_number_of_men
+            $this->withoutExceptionHandling();
+            $this->withoutMiddleware();
+            $response = $this->get("http://localhost:8000/api/auth/totalmen");
+            $response->assertOk();
+     
+        }
+
+         /** @test */
+         public function admin_can_get_total_number_of_women()
+         {
+     // clear && ./vendor/bin/phpunit tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php --filter admin_can_get_total_number_of_women
+             $this->withoutExceptionHandling();
+             $this->withoutMiddleware();
+             $response = $this->get("http://localhost:8000/api/auth/totalwomen");
+             $response->assertOk();
+      
+         }       
+           
 }
