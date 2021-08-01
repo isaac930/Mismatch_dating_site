f9668e80fac8379148fa11eed4f27d4c80d06c6b
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-01 22:45:01 +0300

    added test for admin to get total numbers of users and total number of successfull users

--- tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php
+++ tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php
@@ -8,15 +8,26 @@ use Tests\TestCase;
 
 class Counts_Controller_Tests extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+        /** @test */
+        public function admin_can_get_total_number_of_users()
+        {
+    // clear && ./vendor/bin/phpunit tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php --filter admin_can_get_total_number_of_users
+            $this->withoutExceptionHandling();
+            $this->withoutMiddleware();
+            $response = $this->get("http://localhost:8000/api/auth/totalusers");
+            $response->assertOk();
+     
+        }
 
-        $response->assertStatus(200);
-    }
+               /** @test */
+               public function admin_can_get_total_number_of_successful_profiles()
+               {
+           // clear && ./vendor/bin/phpunit tests/Feature/Http/Controllers/Api/Counts_Controller_Tests.php --filter admin_can_get_total_number_of_successful_profiles
+
+                   $this->withoutExceptionHandling();
+                   $this->withoutMiddleware();
+                   $response = $this->get("http://localhost:8000/api/auth/totalsuccessful_profiles");
+                   $response->assertOk();
+            
+               }
 }
