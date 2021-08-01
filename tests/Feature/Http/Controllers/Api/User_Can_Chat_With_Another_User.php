990402a873b07a155fbe6cf9739059ef9c558420
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-01 18:34:54 +0300

    added a test for a user can get his own posts

--- tests/Feature/Http/Controllers/Api/User_Can_Chat_With_Another_User.php
+++ tests/Feature/Http/Controllers/Api/User_Can_Chat_With_Another_User.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 class User_Can_Chat_With_Another_User extends TestCase
 {
+    // run all tests clear && ./vendor/bin/phpunit tests/Feature/Http/Controllers/Api/User_Can_Chat_With_Another_User.php
 
      /** @test */
      public function a_user_can_chat_with_another_user()
@@ -17,7 +18,8 @@ class User_Can_Chat_With_Another_User extends TestCase
         $this->withoutExceptionHandling();
         $this->withoutMiddleware();
         $response = $this->post("http://localhost:8000/api/auth/chats",[
-          
+         
+               
         'name' => 'kirumira isaac',
         'email' => 'kirumiraisaac@gmail.com',  
         'contact' => '256759939936',
@@ -27,4 +29,30 @@ class User_Can_Chat_With_Another_User extends TestCase
         $response->assertOk();
 
      }
+
+       /** @test */
+       public function a_user_can_update_his_chat_post()
+       {
+        // clear && ./vendor/bin/phpunit tests/Feature/Http/Controllers/Api/User_Can_Chat_With_Another_User.php --filter a_user_can_update_his_chat_post   
+        $id = 4;
+        $this->withoutExceptionHandling();
+        $this->withoutMiddleware();
+        $response = $this->put("http://localhost:8000/api/auth/chats/$id",[
+         
+               
+        'name' => 'kirumira isaac',
+        'email' => 'kirumiraisaac@gmail.com',  
+        'contact' => '256759939936',
+        'chatment_email' => 'oliva@gmail.com',
+        'post' => 'How are you oliva updated ?',
+        ]);
+        $response->assertOk();
+       }
+       
+      /** @test */
+       public function a_user_can_get_his_own_posts()
+       {
+
+
+       }
 }
