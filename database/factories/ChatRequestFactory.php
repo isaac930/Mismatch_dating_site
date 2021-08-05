64f00daedc4b8bbdce687b15ccdef08fe9d3f73d
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-05 20:37:11 +0300

    added factory for chat request

---
+++ database/factories/ChatRequestFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ChatRequest;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChatRequestFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ChatRequest::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
